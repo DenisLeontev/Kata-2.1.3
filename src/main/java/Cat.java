3e55772ca9e39c5883420249714fae0a30201188
DenisLeontev <ldb107@mail.ru>
2022-11-03 13:58:40 +0500

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+
+    private String massage;
+}
